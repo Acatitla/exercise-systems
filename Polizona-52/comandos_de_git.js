72d8c819d3622aa736dc02da9460f4c1875fb622
Angel Acatitla <angelgonzalez477@gmail.com>
2020-12-02 12:12:47 -0600

    comandos de git y json en funcion

---
+++ Polizona-52/comandos_de_git.js
@@ -0,0 +1,21 @@
+// ------------------------------- Comandos -------------------------
+// pwd -> muestra la ruta en donde nos encontramos
+// ls -> muestra qué hay dentro de la ruta donde estoy
+// cd -> cambio de carpeta (cd nombre-carpeta o cd .. nos regresa a una carpeta anterior)
+// touch -> crea un archivo (touch nombre-archivo)
+// mkdir -> crea carpeta (mkdir nombre-carpeta)
+// nano -> editor de texto dentro de la terminal (nano archivo)
+// cat -> nos muestra que contenido tiene un archivo (cat archivo)
+// mv -> mover un archivo de lugar o cambiarle el nombre (mv archivo nueva-locacion o mv archivo nuevo-nombre)
+// rm -> borra archivo o carpeta (rm archivo o rm -r carpeta)
+// rm -rf -> borra todo (mucho cuidado con este comando)
+// clear -> limpia la pantalla
+
+//PARA TRABAJAR CON GITHUB
+// git clone LINK-DEL-CODIGO-EN-GITHUB
+// cd git-NOMBRE-DE-LA-CARPETA
+// git branch su-nombre (CREAS UNA RAMA)
+// git checkout su-nombre (ESTO PARA ACCEDER A SU RAMA)
+// git add ARCHIVO.js (AGREGAS TU ARCHIVO)
+// git commit -m "COMENTARIO DE QUE HICISTE EN EL ARCHIVO"
+// git push origin NOMBRE-DE-TU-RAMA
--- Polizona-52/funcion.php
+++ Polizona-52/funcion.php
@@ -1,59 +1,109 @@
 <html>
-<body>
-<h2>calcular valor total del inventario. sumatoria de cantidad por costo unitario</h2>
+<head>
+		<Title>empresa individual de José Ángel Acatitla González </Title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    	<link href="bootstrap.min.css">
+    	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+		<meta charset = "utf-8">
+	</head>
+	<style>
+	body, html{
+          background-position: center-center;
+          background-size: cover;
+          background-position: center center;
+          font-family: "Poppins";
+          background-color:#38AF79;
+      }
 
-<p id="demo"></p>
-
-<script>var actual1 =0;
-var text = '{"actual":"function() {for( i in obj.inventario){ actual1 += +(obj.inventario[i].embarque.cantidad*obj.inventario[i].embarque.costounitario)}return actual1;}","inventario":[{"embarque":{"cantidad":2,"costounitario":10}},{"embarque":{"cantidad":2,"costounitario":1}},{"embarque":{"cantidad":2,"costounitario":4}},{"embarque":{"cantidad":3,"costounitario":2}},{"embarque":{"cantidad":2,"costounitario":2}}]}';
-//var text = '{"actual":"function() {for( i in obj.costoembarque){ actual1 += +(obj.inventario[i].costo.unitario*obj.inventario[i].embarque.costounitario)}return actual1;}","inventario":[{"embarque":{"cantidad":2,"costounitario":10}},{"embarque":{"cantidad":2,"costounitario":1}},{"embarque":{"cantidad":2,"costounitario":4}},{"embarque":{"cantidad":3,"costounitario":2}},{"embarque":{"cantidad":2,"costounitario":2}}]}';
-//var text = '{"actual":"function() {for( i in obj.inventario){ actual1 += +(obj.inventario[i].embarque.cantidad*obj.inventario[i].embarque.costounitario)}return actual1;}","inventario":[{"embarque":{"cantidad":2,"costounitario":10}},{"embarque":{"cantidad":2,"costounitario":1}},{"embarque":{"cantidad":2,"costounitario":4}},{"embarque":{"cantidad":3,"costounitario":2}},{"embarque":{"cantidad":2,"costounitario":2}}]}';
-var obj = JSON.parse(text);
-obj.actual = eval("(" + obj.actual + ")");
-document.getElementById("demo").innerHTML = obj.actual();
-</script>
-
-<?PHP
-	$hostname_localhost ="68.70.164.26";
-	$database_localhost ="polizona_mercado";
-	$username_localhost ="polizona_estudiante";
-	$password_localhost ="UPIICSAes#1";
-	$json=array();
-				
-		$opcion = $_GET["opciones"];
-		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
-		//$consulta="select idempresa, idtipoalmacen, maximo from almacen;";
-		$consulta="select idalmacen, unidades, costoembarque from embarque where idempresa='52';";
-		$resultado=mysqli_query($conexion,$consulta);
-		if($conexion){
-			echo "<table>";
-			echo "<th> Almacén</th>";
-			echo "<th> Unidades </th>";
-			echo "<th> Costo de Embarque </th>";
-			while($registro=mysqli_fetch_array($resultado)){
-				$result["idalmacen"]=$registro['idalmacen'];
-				$result["unidades"]=$registro['unidades'];
-				$result["costoembarque"]=$registro['costoembarque'];
-				$json['Clasificador'][]=$result;
-				
-				echo "<tr>";
-					echo "<td>".$registro['idalmacen']." </td>";
-					echo "<td>".$registro['unidades']." </td>";
-					echo "<td>".$registro['costoembarque']." </td>";
-				echo "</tr>";
-			}
-			
-			echo "</table>";
-			
-			echo json_encode($json);
-			mysqli_close($conexion);
+		select{
+			font-size:11px;
 		}
-		else{
-			echo "error";
-		}
-		
-			
-?>
 
-</body>
+		.barra{
+			width:20rem; 
+		}
+			
+	</style>
+
+	<body>
+	<div class="container">
+				<div class="row align-items-center">
+					<div class="col-12 align-self-center text-center p-4">
+						<h3>calcular:</h3>
+						<!--
+							var text = '{"actual":"function() {for( i in obj.costoembarque){ actual1 += +(obj.inventario[i].costo.unitario*obj.inventario[i].embarque.costounitario)}return actual1;}","inventario":[{"embarque":{"cantidad":2,"costounitario":10}},{"embarque":{"cantidad":2,"costounitario":1}},{"embarque":{"cantidad":2,"costounitario":4}},{"embarque":{"cantidad":3,"costounitario":2}},{"embarque":{"cantidad":2,"costounitario":2}}]}';
+							var text = '{"actual":"function() {for( i in obj.inventario){ actual1 += +(obj.inventario[i].embarque.cantidad*obj.inventario[i].embarque.costounitario)}return actual1;}","inventario":[{"embarque":{"cantidad":2,"costounitario":10}},{"embarque":{"cantidad":2,"costounitario":1}},{"embarque":{"cantidad":2,"costounitario":4}},{"embarque":{"cantidad":3,"costounitario":2}},{"embarque":{"cantidad":2,"costounitario":2}}]}';
+						<h3>valor total del inventario. Sumatoria de cantidad por costo unitario</h3>
+						-->
+						<h5>Ejemplo: Calcular el precio de Isan. </h5>
+						<h6>el link es: https://rextester.com/BQP78803</h6>
+						<p id="demo"></p>
+						<!--
+						<script>
+						var actual1 = 0;
+						var text = '{"actual":"function() {for( i in obj.inventario){ actual1 += +(obj.inventario[i].embarque.cantidad*obj.inventario[i].embarque.costounitario)}return actual1;}","inventario":[{"embarque":{"cantidad":2,"costounitario":10}},{"embarque":{"cantidad":2,"costounitario":1}},{"embarque":{"cantidad":2,"costounitario":4}},{"embarque":{"cantidad":3,"costounitario":2}},{"embarque":{"cantidad":2,"costounitario":2}}]}';
+						var obj = JSON.parse(text);
+						obj.actual = eval("(" + obj.actual + ")");
+						document.getElementById("demo").innerHTML = obj.actual();
+						</script>
+						-->
+
+						<script>
+						var text = '{"isan":"function() {precio=299500;if(precio<28645201.02){isan=precio*0.02}else{if(precio<66666666668){isan=0;}else {}} return isan;}"}';
+						var obj = JSON.parse(text);
+						obj.isan = eval("(" + obj.isan + ")");
+						document.getElementById("demo").innerHTML = obj.isan();
+						</script>
+						<br>
+						
+						<h2>Nuestro llamado a Base de datos:</h2>
+
+						<?PHP
+							$hostname_localhost ="68.70.164.26";
+							$database_localhost ="polizona_mercado";
+							$username_localhost ="polizona_estudiante";
+							$password_localhost ="UPIICSAes#1";
+							$json=array();
+										
+								$opcion = $_GET["opciones"];
+								$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+								//$consulta="select idempresa, idtipoalmacen, maximo from almacen;";
+								$consulta="select idalmacen, unidades, costoembarque from embarque where idempresa='52';";
+								$resultado=mysqli_query($conexion,$consulta);
+								if($conexion){
+									echo "<table>";
+									echo "<th> Almacén</th>";
+									echo "<th> Unidades </th>";
+									echo "<th> Costo de Embarque </th>";
+									while($registro=mysqli_fetch_array($resultado)){
+										$result["idalmacen"]=$registro['idalmacen'];
+										$result["unidades"]=$registro['unidades'];
+										$result["costoembarque"]=$registro['costoembarque'];
+										$json['Clasificador'][]=$result;
+										
+										echo "<tr>";
+											echo "<td>".$registro['idalmacen']." </td>";
+											echo "<td>".$registro['unidades']." </td>";
+											echo "<td>".$registro['costoembarque']." </td>";
+										echo "</tr>";
+									}
+									
+									echo "</tr>";
+									echo "</tr>";
+									echo "</table>";
+									
+									echo json_encode($json);
+									mysqli_close($conexion);
+								}
+								else{
+									echo "error";
+								}
+								
+									
+						?>
+					</div>
+				</div>	
+			</div>
+	</body>
 </html>
