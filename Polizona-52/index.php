2933c3c104d33919c548f207ba60df04971abf18
Angel Acatitla <angelgonzalez477@gmail.com>
2021-01-25 10:01:20 -0600

    form and conexion

--- Polizona-52/index.php
+++ Polizona-52/index.php
@@ -1,52 +1,141 @@
-<html>
+<html lang="es">
 	<head>
 		<Title>empresa individual de José Ángel Acatitla González </Title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     	<link href="bootstrap.min.css">
-		<meta charset = "utf-8">
+		<meta charset = "UTF-8">
 	</head>
 	<style>
-	body, html{
-          background-color:#38AF79;
-	</style>
-		<body>
-			<div class="container">
+	body{
+          background-color: #38AF79;
+		  text-align: center;
+	}
+	form {
+    display: inline-block;
+	margin: 50px;
+	align: "center";
+	}
+	</style> 
+	<body>
+		<div class="container">
 				<div class="row align-items-center">
 					<div class="col-12 align-self-center text-center p-4">
 						<h1>José Ángel Acatitla González</h1>
 						<h2>Diseño de Sistemas.</h2>
 						<h2>2CM32</h2>
-						<!--<h2>Este es un ejemplo de como subir los archivos desde git bash</h2>
-								<form action="misprocesos.php" method="get">
-								<label>Mi modelo de negocio consta de los siguientes procesos</label><br><br>
-								Selecciona el proceso que deseas ejecutar:
-								<select name="opciones">
-									<option value="A"> Clasificación de objetos</option>
-									<option value="B"> Agrupación de objetos</option>
-								</select><br><br>
-								<input type="submit"/>
-							</form> -->
-							<br>
-							<form action="general_data.html" method="get">
-							<button type="submit">Datos Generales</button>
+						<br>
+						<div class="row container my-4">
+							<form class="mx-50px" action="general_data.html" method="get">
+								<button type="submit">Datos Generales</button>
 							</form>
-							<br>
-							<br>
 							<form action="funcion.php" method="get">
-							<button type="submit">Función</button>
+								<button type="submit">Función</button>
 							</form>
-							<br>
-							<br>
 							<form action="elements.html" method="get">
-							<button type="submit">Ejemplo de: Elementos de objetos</button>
+								<button type="submit">Ejemplo Elementos de objetos</button>
+							</form>
+							<form action="empresa.php" method="get">
+								<button type="submit">Consulta de Industria</button>
 							</form>
 							<br>
-							<br><form action="empresa.php" method="get">
-							<button type="submit">Consulta de Industria</button>
+					
+						</div>
+						<div class ="row text-center">
+							<form action="asiento.php" method="post">
+								<fieldset>
+								<legend> Ingrese Asiento </legend>
+								<p>
+								<label>Empresa:
+								<input type="int" name="idempresa" />
+								</label>
+								</p>
+								<p>
+								<label>Asiento: 
+								<input type="int" name="idasiento" />
+								</label>
+								</p>
+								<p>
+								<label> Monto:  
+								<input type="decimal" name="monto" />
+								</label>
+								</p>
+								<p>
+								<input type="submit" value="enviar"/>
+								</p>
+								</fieldset>
 							</form>
-							<br>
+
+							<form action="cargo.php" method="post">
+								<fieldset>
+								<legend> Ingrese Cargo </legend>
+								<p>
+								<label>Empresa:
+								<input type="int" name="idempresa" />
+								</label>
+								</p>
+								<p>
+								<label>Asiento: 
+								<input type="int" name="idasiento" />
+								</label>
+								</p>
+								<p>
+								<label>Cargo: 
+								<input type="int" name="idcargo" />
+								</label>
+								</p>
+								<p>
+								<label>Cuenta: 
+								<input type="int" name="idcuenta" />
+								</label>
+								</p>
+								<p>
+								<label> Monto:  
+								<input type="decimal" name="monto" />
+								</label>
+								</p>
+								<p>
+								<input type="submit" value="enviar"/>
+								</p>
+								</fieldset>
+							</form>
+
+							<form action="abono.php" method="post">
+								<fieldset>
+								<legend> Ingrese Abono </legend>
+								<p>
+								<label>Empresa:
+								<input type="int" name="idempresa" />
+								</label>
+								</p>
+								<p>
+								<label>Asiento: 
+								<input type="int" name="idasiento" />
+								</label>
+								</p>
+								<p>
+								<label>Abono: 
+								<input type="int" name="idabono" />
+								</label>
+								</p>
+								<p>
+								<label>Cuenta: 
+								<input type="int" name="idcuenta" />
+								</label>
+								</p>
+								<p>
+								<label> Monto:  
+								<input type="float" name="monto" />
+								</label>
+								</p>
+								<p>
+								<input type="submit" value="enviar"/>
+								</p>
+								</fieldset>
+							</form>
+
+						</div>
 					</div>
 				</div>	
 			</div>
-		</body>
+	</body>
 </html>
